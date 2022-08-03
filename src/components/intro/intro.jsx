2fb32831030ada0cae144ed9b1942aedc8ad4be9
MrazTevin <millatevin@gmail.com>
2022-08-03 12:09:02 +0300

    feat: display name and titles

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,7 +4,19 @@ import "./intro.css"
 const Intro = () => {
   return (
     <div className='i'>
-      <div className='i-left'>left</div>
+      <div className='i-left'>
+        <div className='i-left-wrapper'>
+          <h2 className='i-preface'>Hello, My name is</h2>
+          <h2 className='i-name'>Tevin Milla</h2>
+          <div className='i-titles'>
+            <div className="i-title-wrappers">
+              <div className="title-name">Software Engineer</div>
+              <div className="title-name">Cloud DevOps Engineer</div>
+              <div className="title-name">UI/UX Engineer</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className='i-right'>right</div>
     </div>
   )
